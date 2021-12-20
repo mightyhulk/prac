01d33abb413e0212c87acc2f54a1b67fd135fe96
mighty_hulk <coder10978@gmail.com>
2021-12-20 09:59:18 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int i,j;
+
+    cin>>i;
+    if(i%2 == 0){
+        cout <<"The number is even";
+    }
+ 
+    else{
+        cout<<"The entered number is not even";
+    }
+
+  return 0;
+
+}
+
+
+
+
+
+
+
+
---
+++ check.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     char a,e,i,o,u,t, A, E, I ,O , U;
+     cout<<"Enter the character that you want to check";
+     cin>>t;
+ 
+     if(t=='a' || t =='e' || t =='i' || t =='o' || t =='u' ){
+         cout<<"It is a vowel";
+     } 
+     
+     if(t=='A' || t =='E' || t=='I' || t =='O' || t =='U' ){
+         cout<<"It is a vowel";
+     }
+     
+     
+     else{
+         cout<<"It is not a vowel";
+     }
+
+
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
